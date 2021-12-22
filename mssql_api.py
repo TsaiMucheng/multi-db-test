6a98a332f29b2296e64838feb1cc9f5b0b1182a7
MuCheng <49551169+MuTsai2019@users.noreply.github.com>
2021-12-22 23:30:21 +0800

    Update mssql_api.py

--- mssql_api.py
+++ mssql_api.py
@@ -1,28 +1,29 @@
 #read sql Server(pyodbc)
 class MSSQLHandeler:
+    import pyodbc
+    import pandas as pd
     def __init__(self, DriverClient = None, DbHost = None, database = None, username = None, password = None):
-        try:
-            import pyodbc
+        try:            
             self.result = None
             self.params = "DRIVER={" + DriverClient + "}; SERVER=" + DbHost + "; DATABASE=" + database + "; UID=" + username + "; PWD=" + password
-            self.connection = pyodbc.connect(self.params)
+            self.connection = type(self).pyodbc.connect(self.params)
         except Exception as ex:
             print(str(ex))
     def GetDF(self, sql = None):
         try:
-            import pandas as pd
-            rs = pd.read_sql_query(sql, self.connection)
+            rs = type(self).pd.read_sql_query(sql, self.connection)
             self.result = rs
+            self.connection.close()
         except Exception as ex:
             return ex;
         finally:
             return self.result
     def Comand(self, sql = None):
         try:
-            import pandas as pd
             #rs = self.connection.execute(sql).fetchall()
-            rs = pd.read_sql_query(sql, self.connection)
+            rs = type(self).pd.read_sql_query(sql, self.connection)
             self.result = rs
+            self.connection.close()
         except Exception as ex:
             return ex;
         finally:
